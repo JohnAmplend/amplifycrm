0c9c7894cd9725890c92759ecd83162635e17536
John Tugade <john@amplend.net>
2026-01-05 17:16:53 +0000

    File changes

--- src/components/tracker/CardFormModal.jsx
+++ src/components/tracker/CardFormModal.jsx
@@ -117,7 +117,7 @@ export default function CardFormModal({ isOpen, onClose, onSave, card, columns }
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="sm:max-w-lg">
+      <DialogContent className="sm:max-w-lg max-h-[90vh] overflow-y-auto z-[1001]">
         <DialogHeader>
           <DialogTitle>{card ? "Edit Card" : "Add New Card"}</DialogTitle>
         </DialogHeader>
