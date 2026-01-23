2be603f029b11c7888997d0b36ed6d6ece267772
John Tugade <john@amplend.net>
2026-01-23 22:31:06 +0000

    File changes

--- src/components/crm/NeuroCard.jsx
+++ src/components/crm/NeuroCard.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-export default function NeuroCard({ children, className = "", inset = false }) {
+export default function NeuroCard({ children, className = "", inset = false, onClick }) {
   return (
-    <div className={`${inset ? 'ampvibe-inset' : 'ampvibe-card'} p-6 ${className}`}>
+    <div 
+      className={`${inset ? 'ampvibe-inset' : 'ampvibe-card'} p-6 ${className} ${onClick ? 'cursor-pointer hover:scale-[1.01] transition-transform' : ''}`}
+      onClick={onClick}
+    >
       {children}
     </div>
   );
